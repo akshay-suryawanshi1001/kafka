bc8d2bbb798585f22d9fd083ad16881694525d63
akshay-suryawanshi1001 <akshay.suryawanshi1001@gmail.com>
2022-06-26 22:44:41 +0530

    first commit

---
+++ src/main/java/com/akshay/kafka/KafkaListeners.java
@@ -0,0 +1,13 @@
+package com.akshay.kafka;
+
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+public class KafkaListeners {
+
+	@KafkaListener(topics = "akshay", groupId = "groupId")
+	void listener(String data) {
+		System.out.println("listener receiver : " + data);
+	}
+}
